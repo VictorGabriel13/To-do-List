2bb183486eb49e16ee646b6e6cb02b6cdb4d1434
VictorGabriel13 <victorgabrieldelimarocha@gmail.com>
2025-05-06 13:26:15 -0300

    Integração com o Suap IFRN

---
+++ src/app/settings.sample.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = 'cgwnABTFy3peodcOnQ0aeubnt89XPqxRcZhmBx4j';
+var REDIRECT_URI = 'http://localhost:8888/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
